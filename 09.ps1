1bca230264878c1067ba6d35d141fdd161809fef
Carles-Flordelis <95307553+Carles-Flordelis@users.noreply.github.com>
2022-02-11 20:45:06 +0100

    9

--- 09.ps1
+++ 09.ps1
@@ -1,5 +1,7 @@
 #si dos numeros passats per parametre a l'script son iguals mostra OK. Si son diferents mostra KO
-if($0 -eq $1)
+if($args[0] -eq $args[1]){
+    Write-host "OK"}
+else{
     Write-host "KO"
-else 
-    Write-host "OK"
+} 
+    
