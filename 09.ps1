8f29f49e96e471e040cc66251cb2337db34870ef
Carles-Flordelis <95307553+Carles-Flordelis@users.noreply.github.com>
2022-02-11 20:13:22 +0100

    Update 09.ps1

--- 09.ps1
+++ 09.ps1
@@ -1,5 +1,6 @@
 #si dos numeros passats per parametre a l'script son iguals mostra OK. Si son diferents mostra KO
-if($1!=$2)
+if($1 != $2)
     Write-host "KO"
-else Write-host "OK"
+else 
+    Write-host "OK"
 
